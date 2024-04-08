97707c02211b964d20ce597444435f5040fd04e7
Devgum <devgum@foxmail.com>
2024-04-08 13:37:46 +0100

    Add Lab5

---
+++ labs/lab5/exampleScripts/helloworld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hello World</title>
+</head>
+<body>
+	<p>HTML says Hello World</p>
+	<script type="text/javascript">
+		document.write('<p>JavaScript says Hello world.</p>');
+	</script>
+	<?php
+		echo "<p>PHP says Hello World</p>";
+	?>
+</body>
+</html>
