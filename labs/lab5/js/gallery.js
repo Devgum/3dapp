f2b3d42f7c5e5d9924e7f3385100d106b95d4278
Ruochen Zhao <devgum@foxmail.com>
2024-04-11 20:25:38 +0100

    Lab5 update gallery in index.html

--- labs/lab5/js/gallery.js
+++ labs/lab5/js/gallery.js
@@ -3,7 +3,7 @@ var numberOfColumns = 2;
 var htmlCode = "";
 var response;
 
-function gallery(gallery_type, column_number) {
+function gallery(gallery_type) {
     var url = "hook.php";
     var params = "gallery_type=" + gallery_type;
     var send = url + "?" + params
@@ -12,28 +12,19 @@ function gallery(gallery_type, column_number) {
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.readyState == 4) {
             response = xmlHttp.responseText.split(";");
-            htmlCode += '<tr>';
             for (var i=0; i<response.length; i++) {
-                htmlCode += '<td id="gallery_cell">';
                 htmlCode += '<a href="' + response[i] +  '">';
-                htmlCode += '<img src="' + response[i] + '" class="img_thumbnail"/>';
+                htmlCode += '<img src="' + response[i] + '" class="card-img-top img-thumbnail"/>';
                 htmlCode += '</a>';
-                htmlCode += '</td>';
-
-                if (((i+1)%numberOfColumns) == 0) {
-                    htmlCode += '</tr><tr>';
-                }
             }
-            htmlCode += '</tr>';
             document.getElementById('gallery_' + gallery_type).innerHTML = htmlCode;
         }
     }
 }
 
 $(document).ready(function() {
-    var column_number = 2;
     var gallery_types = ['coke', 'sprite', 'pepper'];
     for (var i = gallery_types.length - 1; i >= 0; i--) {
-        gallery(gallery_types[i], column_number);
+        gallery(gallery_types[i]);
     }
 });
