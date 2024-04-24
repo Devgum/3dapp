5aaae2328431bd03cab9eb3802f4711de87e989f
Devgum <devgum@foxmail.com>
2024-04-24 15:20:22 +0100

    Lab6 Update

--- labs/lab6/scripts/js/getJsonData.js
+++ labs/lab6/scripts/js/getJsonData.js
@@ -22,5 +22,21 @@ $(document).ready(function() {
         $('#title_right').html(generateHtmlContent('h2', jsonObj.pageTextData[3].title));
         $('#subtitle_right').html(generateHtmlContent('h3', jsonObj.pageTextData[3].subtitle));
         $('#description_right').html(generateHtmlContent('p', jsonObj.pageTextData[3].description));
+
+        $('#title_left_bottom').html(generateHtmlContent('h2', jsonObj.pageTextData[1].title));
+        $('#subtitle_left_bottom').html(generateHtmlContent('h3', jsonObj.pageTextData[1].subtitle));
+        $('#description_left_bottom').html(generateHtmlContent('p', jsonObj.pageTextData[1].description));
+
+        $('#title_centre_bottom').html(generateHtmlContent('h2', jsonObj.pageTextData[2].title));
+        $('#subtitle_centre_bottom').html(generateHtmlContent('h3', jsonObj.pageTextData[2].subtitle));
+        $('#description_centre_bottom').html(generateHtmlContent('p', jsonObj.pageTextData[2].description));
+
+        $('#title_right_bottom').html(generateHtmlContent('h2', jsonObj.pageTextData[3].title));
+        $('#subtitle_right_bottom').html(generateHtmlContent('h3', jsonObj.pageTextData[3].subtitle));
+        $('#description_right_bottom').html(generateHtmlContent('p', jsonObj.pageTextData[3].description));
+
+        $('#model_description_coke').html(generateHtmlContent('p', jsonObj.pageTextData[4].model_description));
+        $('.camera_title').html(generateHtmlContent('h5', jsonObj.pageTextData[5].camera_title));
+        $('.camera_description').html(generateHtmlContent('p', jsonObj.pageTextData[5].camera_description));
     });
 });
