3df459e2ad981dd46843690f16b7382f72e7008f
Devgum <devgum@foxmail.com>
2024-05-10 12:25:26 +0100

    update lab7

--- labs/lab7/application/model/model.php
+++ labs/lab7/application/model/model.php
@@ -6,22 +6,22 @@ class Model {
         // Simulate the model's data
         return array(
             'model_1' => 'Coke Can 3D Image 1',
-            'image3D_1' => 'coke/coke_1',
+            'image3D_1' => 'coke/coke_1.png',
 
             'model_2' => 'Coke Can 3D Image 2',
-            'image3D_2' => 'coke/coke_2',
+            'image3D_2' => 'coke/coke_2.png',
 
             'model_3' => 'Sprite Bottle 3D Image 1',
-            'image3D_3' => 'sprite/sprite_1',
+            'image3D_3' => 'sprite/sprite_1.jpg',
 
             'model_4' => 'Sprite Bottle 3D Image 2',
-            'image3D_4' => 'sprite/sprite_2',
+            'image3D_4' => 'sprite/sprite_2.png',
 
             'model_5' => 'Dr Pepper Cup 3D Image 1',
-            'image3D_5' => 'pepper/pepper_1',
+            'image3D_5' => 'pepper/pepper_1.jpg',
 
             'model_6' => 'Dr Pepper Cup 3D Image 2',
-            'image3D_6' => 'pepper/pepper_2'
+            'image3D_6' => 'pepper/pepper_2.png'
         );
     }
 }
