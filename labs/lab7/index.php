e6934e2b57b72091d9be5125955207b77d6eb578
Devgum <devgum@foxmail.com>
2024-05-10 12:17:53 +0100

    update Lab7

--- labs/lab7/index.php
+++ labs/lab7/index.php
@@ -1,39 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Mobile 3D App - Lab7 2024</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <!-- Bootstrap with CDN -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
-    <!-- JQuery -->
-    <!-- JQuery is not needed while using Bootstrap 5 -->
-    <script src="scripts/js/jquery-3.7.1.js"></script>
-    <!-- Using Bootstrap.bundle.js instead of import poppers.js directly to avoid the error of missing popper.js.map -->
-    <!-- Reference: https://stackoverflow.com/a/62473968 -->
-    <!-- <script src="js/popper.js"></script> -->
-    <!-- <script src="js/bootstrap.min.js"></script> -->
-    <script src="scripts/js/bootstrap.bundle.min.js"></script>
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <!-- Fonts -->
-    <!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'> -->
-    <!-- <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'> -->
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/5b09a8cf4e.js" crossorigin="anonymous"></script>
-    <!-- X3Dom -->
-    <script src="scripts/js/x3dom-full.js"></script>
-    <!-- Fancy Box -->
-    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" rel="stylesheet"/>
-    <!-- Custom CSS -->
-    <link href="css/custom.css" rel="stylesheet">
-</head>
-<body>
-	<?php
-		echo("hello php");
-	?>
-</body>
-</html>
+<?php
+// You can define any configuration variables here
+
+// Display error message cause by your PHP scripts
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors',1);
+
+// The require or include statement takes all the text/code/markup that exists 
+// in the specified file and copies it into the file that uses the include (or require) statement.
+require 'application/mvc.php';
+?>
