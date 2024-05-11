ab3b37f3a6a0ea779fb36e5e8d7e605d9ac1d3ef
Devgum <devgum@foxmail.com>
2024-05-11 11:06:42 +0100

    Add Lab8

---
+++ labs/lab8/part_c/application/model/createjson.php
@@ -0,0 +1,32 @@
+<?php
+echo '{ "users":[
+            {
+                "firstName":"Martin",
+                "lastName":"White",
+                "joined": {
+                    "month":"July",
+                    "day":1,
+                    "year":1989
+                }
+            },
+            {
+                "firstName":"Zeeshan",
+                "lastName":"Patoli",
+                "joined": {
+                    "month":"January",
+                    "day":12,
+                    "year":2012
+                }
+            },
+            {
+                "firstName": "Ruochen",
+                "lastName": "Zhao",
+                "joined": {
+                    "month": "May",
+                    "day": 12,
+                    "year": 2024
+                }
+            }
+    ]}
+    ';
+?>
