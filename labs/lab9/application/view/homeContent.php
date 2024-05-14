d77cce7107ecde4d0d38d74615d3ff9770b8987d
Devgum <devgum@foxmail.com>
2024-05-14 17:25:40 +0100

    Update homeContent.php

--- labs/lab9/application/view/homeContent.php
+++ labs/lab9/application/view/homeContent.php
@@ -1,10 +1,11 @@
 <div class='container-fluid mt-5'>
     <div class='row'>
         <div class='col-xs-12 col-sm-12'>
-            <div class='main-3d-image' 
-            style="background-image: url(../assets/images/main_3D.jpg); height=200px;">
+            <div class='main-3d-image'>
                 <div class='col-xs-12 col-sm-4'>
-                    
+                    <?php
+                        echo $data;
+                    ?>
                 </div>
             </div>
         </div>
