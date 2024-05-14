a0445df84842543597e282abc84fa25486459a27
Devgum <devgum@foxmail.com>
2024-05-14 19:04:37 +0100

    Update mvc.php

--- labs/lab9/application/mvc.php
+++ labs/lab9/application/mvc.php
@@ -2,10 +2,15 @@
 require 'view/load.php';
 require 'model/model.php';
 require 'controller/controller.php';
-$pageURI =$_SERVER['REQUEST_URI'];
-$pageURI =substr($pageURI,strrpos($pageURI,'index.php')+10);
-    if (!$pageURI)
-        new Controller('home');
-    else
-        new Controller($pageURI);
+$pageURI = $_SERVER['REQUEST_URI'];
+$queryStringStart = strpos($pageURI, '?');
+if ($queryStringStart !== false) {
+    $pageURI = substr($pageURI, 0, $queryStringStart);
+}
+$pageMethod = basename($pageURI);
+
+if (empty($pageMethod))
+    new Controller('home');
+else
+    new Controller($pageMethod);
 ?>
