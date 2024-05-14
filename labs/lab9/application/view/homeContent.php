874724f55bc32465de1931a9b8c31703399a36f1
Devgum <devgum@foxmail.com>
2024-05-14 17:36:48 +0100

    Update homeContent.php

--- labs/lab9/application/view/homeContent.php
+++ labs/lab9/application/view/homeContent.php
@@ -3,9 +3,9 @@
         <div class='col-xs-12 col-sm-12'>
             <div class='main-3d-image'>
                 <div class='col-xs-12 col-sm-4'>
-                    <h2><?php $main_text['title']?></h2>
-                    <h3><?php $main_text['subtitle']?></h3>
-                    <p><?php $main_text['description']?></p>
+                    <h2><?php print_r($main_text['title']);?></h2>
+                    <h3><?php print_r($main_text['subtitle']);?></h3>
+                    <p><?php print_r($main_text['description']);?></p>
                 </div>
             </div>
         </div>
