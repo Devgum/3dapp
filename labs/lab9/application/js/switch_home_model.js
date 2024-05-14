4f6e27fbbbdcc72b587851fbb1462afa6e0562ea
Devgum <devgum@foxmail.com>
2024-05-14 17:08:00 +0100

    Update Lab9

---
+++ labs/lab9/application/js/switch_home_model.js
@@ -0,0 +1,40 @@
+function getXMLHttp() {
+    var xmlHttp = null;
+    try {
+        xmlHttp = new XMLHttpRequest();
+    } catch (e) {
+        try {
+            xmlHttp = new ActiveXObject("Msxml2.xmlHTTP");
+        } catch (e) {
+            try {
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                return xmlHttp;
+            }
+        }
+    }
+    return xmlHttp;
+}
+
+var base_php = "./application/controller/index.php/";
+var home_content = "homeContent";
+var model_content = "modelContent";
+
+function switch_to(content_api) {
+    var xmlHttp = getXMLHttp();
+    var htmlCode = "";
+    var response;
+    var send = base_php + php_path;
+    xmlHttp.open("GET", send, true);
+    xmlHttp.send(null);
+    xmlHttp.onreadystatechange = function() {
+        if(XMLHttpRequest.readyState == 4) {
+            response = xmlHttp.responseText;
+            document.getElementById('content').innerHTML = response;
+        }
+    }
+}
+
+$(document).ready(function() {
+    switch_to(home_content);
+});
---
+++ labs/lab9/application/view/homeContent.php
@@ -0,0 +1,12 @@
+<div class='container-fluid mt-5'>
+    <div class='row'>
+        <div class='col-xs-12 col-sm-12'>
+            <div class='main-3d-image' 
+            style="background-image: url(../assets/images/main_3D.jpg); height=200px;">
+                <div class='col-xs-12 col-sm-4'>
+                    <div ></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
