27bf596a4cb7b8e44c9e66b6cb527f5a36843b35
Ruochen Zhao <devgum@foxmail.com>
2024-05-15 08:42:56 +0100

    Update mainPage.php

--- labs/lab9/application/view/mainPage.php
+++ labs/lab9/application/view/mainPage.php
@@ -4,6 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mobile 3D App - Lab9 2024</title>
+    <script src="application/js/jquery-3.7.1.js"></script>
+    <script src="application/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/5b09a8cf4e.js" crossorigin="anonymous"></script>
+    <script src="application/js/x3dom-full.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+    <script src="application/js/custom.js"></script>
+    <script src="application/js/switch_home_model.js"></script>
     <!-- CSS -->
     <link href="application/css/bootstrap.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" rel="stylesheet"/>
@@ -16,12 +23,6 @@
     <?php include './application/view/footer.php'?>
     <!-- Scripts -->
     <!-- Placing scripts at tail of the body can reduce the lag caused by the scripts loading-->
-    <script src="application/js/jquery-3.7.1.js"></script>
-    <script src="application/js/bootstrap.bundle.min.js"></script>
-    <script src="https://kit.fontawesome.com/5b09a8cf4e.js" crossorigin="anonymous"></script>
-    <script src="application/js/x3dom-full.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
-    <script src="application/js/custom.js"></script>
-    <script src="application/js/switch_home_model.js"></script>
+
 </body>
 </html>
