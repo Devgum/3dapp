719f509151ad4e12306e5270c0e298048994033a
Ruochen Zhao <devgum@foxmail.com>
2024-05-16 08:24:12 +0100

    Update Lab9

--- labs/lab9/application/js/custom.js
+++ labs/lab9/application/js/custom.js
@@ -12,15 +12,21 @@ function toggle_active_btn(btn_id) {
 
 var animating = false;
 var axis_map = {
-    'x': 'RotatorX',
-    'y': 'RotatorY',
-    'z': 'RotatorZ',
+    'x': '1 0 0',
+    'y': '0 1 0',
+    'z': '0 0 1',
 }
+pi = 3.14159
 
-function animate(axis = 'z') {
+function animate(axis = 'z', key_no = 5) {
     animating = !animating;
     if (!axis_map.hasOwnProperty(axis)) axis = 'z';
-    document.getElementById('model__route').setAttribute('fromNode', axis_map[axis]);
+    keyframes = []
+    for (i = 0; i < key_no; i++) {
+        keyframe = axis_map[axis] + ' ' + pi * 0.5 * i;
+        keyframes.push(keyframe);
+    }
+    document.getElementById('model_Rotator').setAttribute('keyValue', keyframes);
     document.getElementById('model__RotationTimer').setAttribute('enabled', animating.toString());
 }
 
